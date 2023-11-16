b8f628be91d7b4b0a46d48fefd9683b963a65b44
zeinismalingga <zein.ismalingga@gmail.com>
2023-11-16 10:06:59 +0800

    add mongodb

--- api/bot.py
+++ api/bot.py
@@ -1,10 +1,9 @@
-# bot.py
 import requests
 import json
 import feedparser
 import ssl
 from flask import Flask, jsonify
-from os.path import join
+from pymongo import MongoClient
 
 if hasattr(ssl, '_create_unverified_context'):
     ssl._create_default_https_context = ssl._create_unverified_context
@@ -14,16 +13,13 @@ rss_feed_urls = ['https://www.upwork.com/ab/feed/topics/rss?securityToken=d56317
                  'https://www.upwork.com/ab/feed/topics/rss?securityToken=d563174036881b0d3a3472af4b66fe610888ee090c48b0fe655700ddfc441eda4e398733d264804b71627d8d4a48eba8cdda988da5470eb73c8424cd5815dbec&userUid=1310828478425313280&orgUid=1310828478429507585&topic=6705447']
 chat_id = '1066141419'
 
-# Load the last stored entry information from a file or database
-# For simplicity, using a file here
-last_entry_info_file = 'last_entry_info.json'
+# Connect to MongoDB
+client = MongoClient(
+    "mongodb+srv://admin:rcqjCOEwOG7SAgzf@rss-upwork.qfjtuk7.mongodb.net/?retryWrites=true&w=majority")
 
-try:
-    with open(join('/tmp', last_entry_info_file), 'r') as file:
-        last_entry_info = json.load(file)
-except (FileNotFoundError, json.JSONDecodeError):
-    # If the file doesn't exist or is corrupted, initialize with an empty dictionary
-    last_entry_info = {}
+# Specify the database and collection
+db = client['rss-upwork']
+collection = db['rss']
 
 
 def send_telegram_message(message):
@@ -55,21 +51,24 @@ def run_bot():
             description = latest_entry.description
 
             # Check if the latest entry is different from the stored one
-            if last_entry_info.get(rss_feed_url) != title:
-                # Remove html tags from description
-                description = description.replace(
-                    '<br />', '\n').replace('<br /><br />', '\n')
+            stored_info = collection.find_one({'rss_feed_url': rss_feed_url})
+            if stored_info and stored_info.get('title') == title:
+                continue
 
-                message = f"<b>Title</b>: {title}\n<b>Description</b>: {description}\n\n<b>Link</b>: {link}"
-                telegram_response = send_telegram_message(message)
-                print(telegram_response.text)
+            # Remove html tags from description
+            description = description.replace(
+                '<br />', '\n').replace('<br /><br />', '\n')
 
-                # Update the stored information for this RSS feed
-                last_entry_info[rss_feed_url] = title
+            message = f"<b>Title</b>: {title}\n<b>Description</b>: {description}\n\n<b>Link</b>: {link}"
+            telegram_response = send_telegram_message(message)
+            print(telegram_response.text)
 
-    # Save the updated last entry information
-    with open(join('/tmp', last_entry_info_file), 'w') as file:
-        json.dump(last_entry_info, file, indent=2)
+            # Update or insert the stored information for this RSS feed
+            collection.update_one(
+                {'rss_feed_url': rss_feed_url},
+                {'$set': {'title': title}},
+                upsert=True
+            )
 
     return jsonify({'status': 'success', 'message': 'Bot script executed successfully.'})
 
